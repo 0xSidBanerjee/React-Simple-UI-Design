7871faf0f6fce5d8979a0178bd322377c1d37a56
Sidoryx <siddhantban02@gmail.com>
2021-10-27 10:11:23 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
+import React from 'react';
+import practice from './practice.svg';
+import ide from './ide.svg'
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+         <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </div>
+      <div className="hi-user">Hi Username.</div>
+
+      <header className="App-header">
+        <div className="Row">
+          <img src={ide} className="ide-image" alt="ide-image" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu mauris maximus, porttitor mi nec.
+          </p>
+          <Button variant="primary">Open Online IDE</Button>{' '}
+        </div>
+        <div><br></br></div>
+        <div className="Row">
+          <img src={practice} className="App-image" alt="app-image" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu mauris maximus, porttitor mi nec.
+          </p>
+          <Button variant="primary">Start Practicing</Button>{' '}
+        </div>
+        
       </header>
     </div>
   );
